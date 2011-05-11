3e77cbddcbf60d5a22122aef360fe766cbfa8640
Dmitry Rozhkov <dmitry.rozhkov@nokia.com>
2011-05-11 16:09:34 +0300

    new records can have predefined non-integer IDs (in Riak)

--- src/boss/boss_db.erl
+++ src/boss/boss_db.erl
@@ -164,9 +164,11 @@ execute(Commands) ->
 %% Performs validation first; see `validate_record/1'.
 save_record(Record) ->
     case validate_record(Record) of
-        ok -> 
-            IsNew = ( (Record:id() =:= 'id') or (is_integer(Record:id())) ),
-            OldRecord = find(Record:id()),
+        ok ->
+            {IsNew, OldRecord} = case find(Record:id()) of
+                {error, _Reason} -> {true, Record};
+                FoundOldRecord -> {false, FoundOldRecord}
+            end,
             boss_record_lib:run_before_hooks(Record, IsNew),
             case gen_server:call(boss_db, {save_record, Record}, ?DEFAULT_TIMEOUT) of
                 {ok, SavedRecord} ->
